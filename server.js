0a6f8b9b4b9b85047ec3171be098f40fb48db63c
Syed Taha <tahasyed243@gmail.com>
2025-12-19 22:34:51 +0500

    Fix: Add '0.0.0.0' for Railway production

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://parking-app-stk-frontend.vercel.app',
+    credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/spots", spotRoutes);

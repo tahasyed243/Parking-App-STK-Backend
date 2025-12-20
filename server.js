53c3fc8ca6d575ce7991393efcd091e9b36e347c
Syed Taha <tahasyed243@gmail.com>
2025-12-20 16:39:11 +0500

    Fix package.json and server.js for Railway deployment

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
